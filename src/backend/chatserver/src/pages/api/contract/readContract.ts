15d13deda26c84e15b2ee51da060b5cab4266920
김수찬 <tncks097@naver.com>
2023-08-18 02:04:08 +0900

    Feat: 카카오페이 결제 버튼 추가

--- src/backend/chatserver/src/pages/api/contract/readContract.ts
+++ src/backend/chatserver/src/pages/api/contract/readContract.ts
@@ -32,6 +32,29 @@ const receiveData = withCors(async (req: any, res: any) => {
         return;
     }
 
+    if (req.method === 'POST') {
+
+        const {contractId} = req.body;   
+          // const contract = await uploadToS3('contract', image_consumer.originalname, image_consumer.buffer);
+        const contract_id = contractId;
+
+        //   const {conditionQuery, values} = buildConditionQuery(id, ' AND ');
+          
+        //   const NewConstractData = { payed : 1 } // -1 : 대여 중
+          
+          const result = await updateContract({ payed : 1 }, contract_id);
+
+          // const contractData = await readQuery('history',{conditionQuery, values})
+          if (!result){
+            res.status(500).json({ message: 'contract failed cuz of server error' });
+            return;
+          }
+          // 이미지 저장하는거 추가해야함
+          res.status(200).json({ payed : 1, message: 'contract finished' });
+          return
+        }
+        
+
     return res.status(500).json({ error: 'cannot catch methods...' });
 });
 
